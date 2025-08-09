6fa9d89629f7a7bbc686c5fc4042df7d1f5825e7
Saveliy <savarb10@gmail.com>
2025-08-09 14:40:23 +0300

    Some changes from flake8

--- Calculator_v2.py
+++ Calculator_v2.py
@@ -1,68 +1,67 @@
-import os
-from history_manager import *
+from history_manager import show_history, save_history, clear_history, History
 import operations
 
 print(f'Доступные операции: {", ".join(operations.Operations.keys())}')
 
 while True:
-	
-	try:
 
-		# Блок ввода чисел с валидацией
-		nums = []
-		for i in range(2):
-			while True:
-				try:
-					num = float(input(f'Введите число {i+1}: '))
-					nums.append(num)
-					break
-				except ValueError:
-					print("Ошибка: введено не число. Пожалуйста, введите число.")
-	                
-		# Ввод пользователем операции и перевод в нижний регистор
-		op = input('Введите операцию: ').lower().strip()
+    try:
 
-		# Вывод истории операций
-		if op == 'история':
-			show_history()
-			continue
+        # Блок ввода чисел с валидацией
+        nums = []
+        for i in range(2):
+            while True:
+                try:
+                    num = float(input(f'Введите число {i+1}: '))
+                    nums.append(num)
+                    break
+                except ValueError:
+                    print("Ошибка: введено не число. Пожалуйста, введите число.")
+                    
+        # Ввод пользователем операции и перевод в нижний регистор
+        op = input('Введите операцию: ').lower().strip()
 
-		# Сохранение истории в файл
-		if op == 'сохранить':
-			save_history()
-			continue
+        # Вывод истории операций
+        if op == 'история':
+            show_history()
+            continue
 
-		# Очистка истории
-		if op == 'очистить':
-			clear_history()
-			print('История очищена')
-			continue
+        # Сохранение истории в файл
+        if op == 'сохранить':
+            save_history()
+            continue
 
-		# Обработка ошибки когда пользователь ввел несуществующую операцию
-		if op not in operations.Operations:
-			raise KeyError('Недоступная операция')
+        # Очистка истории
+        if op == 'очистить':
+            clear_history()
+            print('История очищена')
+            continue
 
-		# Обработка ошибки деления на ноль
-		if op == 'деление' and nums[1] == 0:
-			print('Пожалуйста, введите второе число отличное от нуля')
-			while nums[1] == 0:
-				nums[1] = float(input('Введите второе число заново: '))
+        # Обработка ошибки когда пользователь ввел несуществующую операцию
+        if op not in operations.Operations:
+            raise KeyError('Недоступная операция')
 
-		# Подсчет результата
-		result = operations.Operations[op](*nums)
+        # Обработка ошибки деления на ноль
+        if op == 'деление' and nums[1] == 0:
+            print('Пожалуйста, введите второе число отличное от нуля')
+            while nums[1] == 0:
+                nums[1] = float(input('Введите второе число заново: '))
 
-		# Вывод результат с округлением
-		print(f'Результат: {result:.2f}' if result % 1 else f'Результат: {int(result)}')
+        # Подсчет результата
+        result = operations.Operations[op](*nums)
 
-		# Сохранение операции в историю
-		str_history = f'{nums[0]} {operations.Operations_Symbols[op]} {nums[1]} = {result:.2f}'
-		History.append(str_history)
+        # Вывод результат с округлением
+        print(f'Результат: {result:.2f}' if result % 1 else f'Результат: {int(result)}')
 
-		#Ограничение истории до 5 операций
-		if len(History) > 5:
-			del History[0]
+        # Сохранение операции в историю
+        str_history = f'{nums[0]} {operations.Operations_Symbols[op]} {nums[1]} = {result:.2f}'
+        History.append(str_history)
 
-	# Вывод ошибок
-	except Exception as e:
-		print(f'Ошибка: {str(e)}')
-		print('Пожалуйста, повторите снова')
+        # Ограничение истории до 5 операций
+        if len(History) > 5:
+            del History[0]
+
+    # Вывод ошибок
+    except Exception as e:
+        print(f'Ошибка: {str(e)}')
+        print('Пожалуйста, повторите снова')
--- history_manager.py
+++ history_manager.py
@@ -1,37 +1,38 @@
 import os
+
 # Функция вывода истории
 def show_history():
-		if not History:
-			print('История пуста')
-		else:
-			print('Последние 5 операций:')
-			for i, record in enumerate(History[::-1], start = 1):
-				print(f'[{i}] {record}')
+    if not History:
+        print('История пуста')
+    else:
+        print('Последние 5 операций:')
+        for i, record in enumerate(History[::-1], start=1):
+            print(f'[{i}] {record}')
 
 # Фукнция сохранения истории
 def save_history():
-	try:
-		with open('history.txt', 'w') as file:
-			for record in History:
-				file.write(record + '\n')
-			print('История сохранена в файл history.txt')
-	except PermissionError:
-			print("Ошибка: нет прав на запись файла")
-	except OSError as e:
-			print(f"Ошибка ОС: {str(e)}")
+    try:
+        with open('history.txt', 'w') as file:
+            for record in History:
+                file.write(record + '\n')
+            print('История сохранена в файл history.txt')
+    except PermissionError:
+        print("Ошибка: нет прав на запись файла")
+    except OSError as e:
+        print(f"Ошибка ОС: {str(e)}")
 
 # Функция очистки истории
 def clear_history():
-	History = []
-	with open('history.txt', 'w'):
-		pass
+    with open('history.txt', 'w'):
+        pass
+
 
 History = []
 
 # Чтение истории
 if os.path.exists('history.txt'):
-	with open('history.txt', 'r') as file:
-		lines = file.readlines()
-		History = [line.strip() for line in lines[-5:]]
+    with open('history.txt', 'r') as file:
+        lines = file.readlines()
+        History = [line.strip() for line in lines[-5:]]
 else:
-	History = []
+    History = []
